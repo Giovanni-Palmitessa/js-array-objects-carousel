fe8c23c5eef40cbe1b3e896b5bfe0442e2536208
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-02 15:26:50 +0200

    add event listenr to btns

--- script/script.js
+++ script/script.js
@@ -3,3 +3,20 @@ const listHightlighted = document.querySelectorAll('.highlighted img');
 const btnPrev = document.querySelector('.btn-up');
 const btnNext = document.querySelector('.btn-down');
 
+let activeIndex = 0;
+
+btnNext.addEventListener('click', function() {
+    listHightlighted[activeIndex].classList.remove('active');
+
+    activeIndex++
+
+    listHightlighted[activeIndex].classList.add('active');
+});
+
+btnPrev.addEventListener('click', function() {
+    listHightlighted[activeIndex].classList.remove('active');
+
+    activeIndex--
+
+    listHightlighted[activeIndex].classList.add('active');
+})
